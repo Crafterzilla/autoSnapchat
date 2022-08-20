ac7bbb224ab0da0777640dee99b663bd09c73939
Crafterzilla <osmarrojas69@gmail.com>
2022-08-20 16:30:07 -0400

    Complete add phone profile function

--- include/menu.py
+++ include/menu.py
@@ -1,6 +1,6 @@
 import pyautogui
 import os
-
+from findIcons import getIcons
 
 
 def addPhone():
@@ -9,6 +9,7 @@ def addPhone():
         phoneList = file.readlines()
         phoneName = ""
 
+        #all of this to get phone name bruh
         while True:
             isNameInList = True
             print("Warning: Spaces, Apostrophes, and quotation marks are not allowed")
@@ -42,15 +43,47 @@ def addPhone():
                 if choice == "Y":
                     break
     
+    #make directories need for program to function
     with open("data/phones.txt", "a") as file:
         file.write("\n" + phoneName)
         os.mkdir("phones/%s" % phoneName)
         os.mkdir("phones/%s/icons" % phoneName)
         os.mkdir("phones/%s/Screenshots" % phoneName)
 
-    
+    #get file for program to function
+    cwd = os.getcwd() + "/phones/" + phoneName
+    print("""
+For the program to work, you need to take four seperate screenshots
+Those screenshots need to look as nearly closely similar as the ones shown in this directory:
+{}/exampleScreenShots
 
+The files need to be put in exactly the order shown and with exactly the name phone1.png, phone2.png, etc.
+The program will extract all the pixel data needed for the bot to find right coordinates
+Place those files into this directory:
+
+{}/Screenshots
+""".format(cwd, cwd))
+
+    input("Once done press enter to continue and to check for files: ")
     
+    areFilesThere = False
+    while areFilesThere == False:
+        for i in range (1, 5):
+            try:
+                file = open("phones/{}/Screenshots/phone{}.png".format(phoneName, str(i)), "r")
+                file.close()
+                areFilesThere = True
+            except:
+                print("Failed to open phone{}.png".format(str(i)))
+                print("Retry placing the approperiate files into {}/Screenshots".format(cwd))
+                input("Once done press enter to retry: ")
+                areFilesThere = False
+                break
+        print()
+
+    print("All files found, now creating all other necessary files...")
+    getIcons(phoneName)
+    print("{} profile created successfully!".format(phoneName))
 
 
 
