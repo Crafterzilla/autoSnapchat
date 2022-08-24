9220a7ba498b3ea222593f7fb97c7aeacb2599a6
Crafterzilla <osmarrojas69@gmail.com>
2022-08-23 21:06:42 -0400

    Add ability to send chats at specified time

--- include/autoMenu.py
+++ include/autoMenu.py
@@ -3,6 +3,7 @@ import keyboard
 from PIL import Image
 from phoneConfigMenu import listPhones
 import time
+from datetime import datetime
 
 class icon:
     memories = 0 #
@@ -121,6 +122,7 @@ If you understand of all of the above, press enter to start autoSnapchat: """)
     pos[icon.chatBox] = findCord("phones/{}/icons/chatBoxIcon.png".format(phoneName))
 
     pyautogui.click(pos[icon.camera].x, pos[icon.camera].y, interval=1)
+    time.sleep(0.5)
     pyautogui.click(pos[icon.takeSnap].x, pos[icon.takeSnap].y, interval=1)
 
     pos[icon.send] = findCord("phones/{}/icons/sendIcon.png".format(phoneName))
@@ -129,8 +131,8 @@ If you understand of all of the above, press enter to start autoSnapchat: """)
     pyautogui.click(pos[icon.arrowBack].x, pos[icon.arrowBack].y, interval=1)
     pyautogui.click(pos[icon.cancel].x, pos[icon.cancel].y, interval=1)
 
-
-    print("To shutdown the program, press crtl + c")
+    print("AutoSnapchat Start!!")
+    print("To shutdown the program, press move mouse to the top-right corner of screen or type ctrl + c into terminal")
     return pos
 
 def spamChats(victimNames, phoneName, textToSend):
@@ -149,6 +151,116 @@ def spamChats(victimNames, phoneName, textToSend):
             pyautogui.press("enter")
     else:
         print("Spam only works for one user")
+
+def printCurrentTime():
+    #print current time in HH:MM AM/PM Time
+    currentTime = datetime.now()
+    currentTime = str(currentTime.time())
+
+    tmpCurrent = ""
+    for i in range(0, 5):
+        tmpCurrent = tmpCurrent + currentTime[i]
+
+    currentTime = tmpCurrent
+
+    hour = int(currentTime[0] + currentTime[1])
+
+    if hour == 0:
+        newHour = 12
+        currentTime = currentTime.replace(str(hour), str(newHour))
+        currentTime += " AM"
+    elif hour > 12:
+        newHour = hour - 12
+        currentTime = currentTime.replace(str(hour), str(newHour))
+        currentTime += " PM"
+    else:
+        currentTime += " AM"
+    print("Current time is {}\n".format(currentTime))
+
+def getTimes():
+    printCurrentTime()
+    #Get times with very precise error checking
+    times = []
+    while True:
+        while True:
+            inputTime = input("Type in time you wish to send in (HH:MM AM) or (HH:MM PM) format: ")
+            inputTime = inputTime.upper()
+
+            try:
+                hour = int(inputTime[0] + inputTime[1])
+                min = int(inputTime[3] + inputTime[4])
+                if len(inputTime) != 8 or inputTime[2] != ":":
+                    print("Invaild format. Make sure to type in like this ex.(07:23 PM)")
+                elif "PM" not in inputTime and "AM" not in inputTime:
+                    print("You need to write either PM or AM at the end of the time")
+                elif hour > 12 or min >= 60:
+                    print("Type in a valid time")
+                else:
+                    times.append(inputTime)
+                    break
+            except ValueError:
+                print("Please type in an integer for HH or MM")
+
+        choice = input("Do you wish to add another time? y/n: ")
+        choice = choice.upper()
+        if choice == "N":
+            break
+    
+
+    for i, timeString in enumerate(times):
+        hour = int(timeString[0] + timeString[1])
+        min = int(timeString[3] + timeString[4])
+
+        if "PM" in timeString:
+            hour += 12
+        elif "AM" in timeString and hour == 12:
+            hour = 0
+        
+        times[i] = (hour, min)
+
+    return times
+    
+def sendChatsAtSetTimes(victimNames, phoneName, textToSend):
+    timesToSend = getTimes()
+
+    while True:
+        choice = input("Spam for a minute or send message once (1 or 2): ")
+        if choice == "1" or choice == "2":
+            break
+        else:
+            print("Invaild input. Choose 1 or 2")
+
+    pos = getCords(phoneName)
+
+    print(timesToSend)
+    currentTime = datetime.now()
+    currentTime = str(currentTime.time())
+    currentHour = int(currentTime[0] + currentTime[1])
+    currentMinute = int(currentTime[3] + currentTime[4])
+
+    print("{}:{}".format(currentHour, currentMinute))
+
+    printCurrentTime()
+    while True:
+        currentTime = datetime.now()
+        currentTime = str(currentTime.time())
+        currentHour = int(currentTime[0] + currentTime[1])
+        currentMinute = int(currentTime[3] + currentTime[4])
+
+        for i, userTime in enumerate(timesToSend):
+            if userTime[0] == currentHour and userTime[1] == currentMinute:
+                for j, victim in enumerate(victimNames):
+                    pyautogui.click(pos[icon.search].x, pos[icon.search].y, interval=0.75)
+                    pyautogui.write(victim, interval=0.25)
+                    time.sleep(1.0)
+                    pyautogui.click(pos[icon.usernameBox][0], pos[icon.usernameBox][1], interval=0.75)
+                    time.sleep(0.5)
+                    pyautogui.write(textToSend, interval=0.25)
+                    pyautogui.press("enter")
+                    pyautogui.click(pos[icon.arrowBack].x, pos[icon.arrowBack].y, interval=0.5)
+                    pyautogui.click(pos[icon.cancel].x, pos[icon.cancel].y, interval=0.5)
+                if choice == "2":
+                    time.sleep(60)
 
 
 def autoSendChats():
@@ -178,7 +290,7 @@ How do you want to auto send texts?
                 # autoSendChats()
                 break
             case send.setTime:
-                # addPhone()
+                sendChatsAtSetTimes(victimNames, phoneName, textToSend)
                 break
             case send.spamThem:
                 spamChats(victimNames, phoneName, textToSend)
